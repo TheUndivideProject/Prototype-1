6fff13ea88f307b717dac4be24586c3251ee0cd3
Trey Roark <rgr45@georgetown.edu>
2025-03-04 11:51:58 -0500

    Restructuring: Adding expander about SEED at the bottom to preserve conciseness

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -18,7 +18,17 @@ This website aims to provide insights into philanthropic spending related to env
 
 """)
 
-with st.expander("About The Dashboard"):
+
+
+st.header("About Each Dashboard")
+st.markdown("""
+-   **Corporate**: Explore corporate giving through our corporate portion of the dashboard. It documents insights from financial and philanthropic metrics from the Securities and Exchange Commission (SEC) financial statements of public companies.
+
+-   **Nonprofit**: Navigate current nonprofit spending through our nonprofit dashboard documents. This includes insights from financial and philanthropic metrics from the IRS Exempt Organizations Business Master File and Form 990 filings of tax-exempt organizations.
+""")
+
+
+with st.expander("🌱 More About SEED 🌱"):
     st.markdown("""
                 
     Transparency is our number one goal. Although current data is accessible via the SEC and IRS, the breadth and depth of this data makes it extremely difficult for those at large without much data science experience to analyze accountability of how philanthropic giving operates in cities of interest and whether this philanthropic giving is reported. This guts the ideal of a feedback loop between the donors that are giving, and the impacted communities that are supposed to receive assistance.
@@ -30,16 +40,6 @@ with st.expander("About The Dashboard"):
     Utlimately, this will operate as a multi-faceted tool that combines education, transparency, and decision-making. Through the education of communities at large, we can find accoutnability and decision-making for government agencies involved not only to make sure impacted communities are served, but to hold those accountable for their philanthropic giving. This innovative dashboard will serve as a convenient platform for direct verification of funding impacts, empowering communities to track and assess the resources flowing into their areas.
 
                 """)
-
-
-st.header("About Each Dashboard")
-st.markdown("""
--   **Corporate**: Explore corporate giving through our corporate portion of the dashboard. It documents insights from financial and philanthropic metrics from the Securities and Exchange Commission (SEC) financial statements of public companies.
-
--   **Nonprofit**: Navigate current nonprofit spending through our nonprofit dashboard documents. This includes insights from financial and philanthropic metrics from the IRS Exempt Organizations Business Master File and Form 990 filings of tax-exempt organizations.
-""")
-
-
 
 ###################
 # SIDEBAR CONTENT #
